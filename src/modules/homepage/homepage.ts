916a7fa2cdf3ad3904cc31706937a194c7458e7c
marerma <maria.kadantseva@gmail.com>
2023-11-02 17:40:44 +0300

    feat: add headers to get requests

--- src/modules/homepage/homepage.ts
+++ src/modules/homepage/homepage.ts
@@ -1,7 +1,7 @@
 import { addElement } from '../../utils/helpers';
 import { Component } from '../component';
 import html from './homepage.tpl.html';
-
+import { userService } from '../../services/user.service';
 import { ProductList } from '../productList/productList';
 
 class Homepage extends Component {
@@ -14,10 +14,11 @@ class Homepage extends Component {
     this.popularProducts.attach(this.view.popular);
   }
 
-  render() {
+  async render() {
+    const userId = await userService.getId();
     fetch('/api/getPopularProducts', {
         headers: {
-          'x-userid': window.userId,
+          'x-userid': userId,
         }
   })
       .then((res) => res.json())
